b0e527455877b198356ddcd6cf0a6549340bf823
mauro <mauro.viader@coneptum.com>
2025-12-04 09:51:25 +0100

    wip buses

--- tests/test_get_users.py
+++ tests/test_get_users.py
@@ -1,20 +1,26 @@
 from infra import MemoryUsersRepo
-from app import GetUsersHandler
+from app import GetUsers, GetUsersHandler
 from domain import User
-
+from infra import QueryBus
 
 
 def test_get_users_1():
+    q_bus = QueryBus()
     repo_user = MemoryUsersRepo([])
-    cmd = GetUsersHandler(repo_user)
-    users = cmd.execute()
+    h = GetUsersHandler(repo_user)
+    q = GetUsers()
+    q_bus.subscribe(GetUsers,h)
+    users = q_bus.dispatch(q)
     assert len(users) == 0
     
 def test_get_users_2():
+    q_bus = QueryBus()
     u = User('1', 'u1', 'u1@test.com', '12345678A')
     repo_user = MemoryUsersRepo([u])
-    cmd = GetUsersHandler(repo_user)
-    users = cmd.execute()
+    h = GetUsersHandler(repo_user)
+    q = GetUsers()
+    q_bus.subscribe(GetUsers,h)
+    users = q_bus.dispatch(q)
     assert len(users) == 1
     assert users[0] == u
 
