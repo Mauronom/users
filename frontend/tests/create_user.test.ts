9c18e1467068bacced8d1cb6ca3c7ae3f0d58798
mauro <mauro.viader@coneptum.com>
2025-12-08 20:36:15 +0100

    wip frontend tests

---
+++ frontend/tests/create_user.test.ts
@@ -0,0 +1,31 @@
+import { MemoryUserRepo } from '../infra/repos'
+import { CreateUser } from '../app/create_user'
+import { User } from '../app/user'
+
+describe('Create User Tests', () => {
+    it('test 1',() => {
+        let screen_data = {username:'user1',email:'user@test.com',dni:'12345678A', result:''}
+        let user_repo = new MemoryUserRepo([])
+        let cmd = new CreateUser(user_repo, screen_data)
+        cmd.execute()
+        expect(screen_data.result).toBe("L'usuari user1 s'ha creat correctament")
+        expect(user_repo.find_all().length).toBe(1)
+        expect(user_repo.find_all()[0].username).toBe('user1')
+    })
+    it('test 2',() => {
+        let screen_data = {username:'user1',email:'usertest.com',dni:'12345678A', result:''}
+        let user_repo = new MemoryUserRepo([])
+        let cmd = new CreateUser(user_repo, screen_data)
+        cmd.execute()
+        expect(screen_data.result).toBe("Email incorrecte")
+        expect(user_repo.find_all().length).toBe(0)
+    })
+    it('test 2',() => {
+        let screen_data = {username:'user1',email:'user@test.com',dni:'12345678A', result:''}
+        let user_repo = new MemoryUserRepo([new User('user1','user@test.com','12345678A')])
+        let cmd = new CreateUser(user_repo, screen_data)
+        cmd.execute()
+        expect(screen_data.result).toBe("l'usuari user1 ja existeix")
+        expect(user_repo.find_all().length).toBe(1)
+    })
+})
