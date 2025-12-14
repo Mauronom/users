b438da9b8bbde5863458d82de1b003e4dbe30113
mauro <mauro.viader@coneptum.com>
2025-12-14 16:01:56 +0100

    added vue

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   testEnvironment: 'jsdom',
+
   transform: {
-    '^.+\\.(ts|js)$': 'babel-jest',
-  //  '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.ts$': 'babel-jest',
+    '^.+\\.vue$': '@vue/vue2-jest',
   },
+
   moduleFileExtensions: ['ts', 'js', 'vue'],
-  testMatch: ['**/*.test.ts'],
+
+  testMatch: ['**/tests/**/*.test.ts'],
+
+  moduleNameMapper: {
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+  },
+
   modulePathIgnorePatterns: ['dist', 'node_modules'],
 }
--- frontend/suma.test.ts
+++
@@ -1,7 +0,0 @@
-import { suma } from "./suma"
-
-describe('A', () => {
-    it('a',() => {
-        expect(suma(2,3)).toBe(5)
-    })
-})
