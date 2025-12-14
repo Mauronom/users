9465c0482485f0a3cd0d4970429a4b2a886185e1
mauro <mauro.viader@coneptum.com>
2025-12-14 16:35:56 +0100

    wip vue

---
+++ frontend/infra/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+      <h1>Hola, Vue 2 amb TypeScript!</h1>
+      <CreateUser />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import Vue from 'vue'
+  import CreateUser from './CreateUser.vue'
+  
+  export default Vue.extend({
+    name: 'App',
+    components: {
+      CreateUser,
+    },
+  })
+  </script>
+  
+  <style>
+  #app {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
+  
---
+++ frontend/infra/CreateUser.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="inc">{{ value }}</button>
+  </template>
+  
+  <script lang="ts">
+  import Vue from 'vue'
+  
+  export default Vue.extend({
+    data() {
+      return { value: 0 }
+    },
+    methods: {
+      inc() {
+        this.value++
+      },
+    },
+  })
+  </script>
+  
