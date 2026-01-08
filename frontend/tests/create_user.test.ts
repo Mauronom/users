33e0b6c59421a04dfc5937e70793a57ca7b6e402
mauro <mauro.viader@coneptum.com>
2026-01-08 10:59:25 +0100

    wip

--- frontend/tests/create_user.test.ts
+++ frontend/tests/create_user.test.ts
@@ -6,38 +6,44 @@ import { v4 as uuid_gen } from 'uuid';
 
 describe('Create User Tests', () => {
 
-    it('test 1',() => {
+    it('test 1',async () => {
         let screen_data = {username:'user1',email:'user@test.com',dni:'12345678A', result:'', message_color:'black'}
         let user_repo = new MemoryUserRepo([])
         let cmd = new CreateUser(user_repo, screen_data, uuid_gen)
-        cmd.execute()
+        let exec = cmd.execute()
+        expect(exec).toBeInstanceOf(Promise)
+        await exec
         expect(screen_data.result).toBe("L'usuari user1 s'ha creat correctament")
         expect(screen_data.message_color).toBe("black")
-        expect(user_repo.find_all().length).toBe(1)
-        expect(user_repo.find_all()[0].username).toBe('user1')
+        expect((await user_repo.find_all()).length).toBe(1)
+        expect((await user_repo.find_all())[0].username).toBe('user1')
         screen_data.username='user2'
         screen_data.email='user2@test.com',
         screen_data.dni='22345678A'
-        cmd.execute()
-        expect(user_repo.find_all().length).toBe(2)
+        await cmd.execute()
+        expect((await user_repo.find_all()).length).toBe(2)
 
     })
-    it('test 2',() => {
+    it('test 2', async () => {
         let screen_data = {username:'user1',email:'usertest.com',dni:'12345678A', result:'', message_color:'black'}
         let user_repo = new MemoryUserRepo([])
         let cmd = new CreateUser(user_repo, screen_data, uuid_gen)
-        cmd.execute()
+        let exec = cmd.execute()
+        expect(exec).toBeInstanceOf(Promise)
+        await exec
         expect(screen_data.result).toBe("Email incorrecte")
         expect(screen_data.message_color).toBe("red")
-        expect(user_repo.find_all().length).toBe(0)
+        expect((await user_repo.find_all()).length).toBe(0)
     })
-    it('test 2',() => {
+    it('test 3', async () => {
         let screen_data = {username:'user1',email:'user@test.com',dni:'12345678A', result:'', message_color:'black'}
         let user_repo = new MemoryUserRepo([new User('uuid','user1','user@test.com','12345678A')])
         let cmd = new CreateUser(user_repo, screen_data, uuid_gen)
-        cmd.execute()
+        let exec = cmd.execute()
+        expect(exec).toBeInstanceOf(Promise)
+        await exec
         expect(screen_data.result).toBe("l'usuari user1 ja existeix")
         expect(screen_data.message_color).toBe("red")
-        expect(user_repo.find_all().length).toBe(1)
+        expect((await user_repo.find_all()).length).toBe(1)
     })
 })
--- frontend/tests/get_users.test.ts
+++ frontend/tests/get_users.test.ts
@@ -3,37 +3,46 @@ import {MemoryUserRepo} from "../infra/repos"
 import {User} from "../domain/user"
 
 describe ('Get User Tests', ()=>{
-    it("test 1: GetUsers with empty user repo returns empty array", ()=>{
+    it("test 1: GetUsers with empty user repo returns empty array", async ()=>{
         let user_repo = new MemoryUserRepo([]);
         let screen_data = {users_list:''}
         let q = new GetUsers(user_repo,screen_data);
-        let users = q.execute();
+        let exec = q.execute()
+        expect(exec).toBeInstanceOf(Promise)
+        await exec
+        let users = await exec;
         expect(users.length).toBe(0);
         expect(screen_data.users_list).toBe('');
     });
 
-    it("test 2: GetUsers returns the right user", ()=>{
+    it("test 2: GetUsers returns the right user", async ()=>{
         let u = new User("1", "u1", "u1@test.com", "12345678A");
         let screen_data = {users_list:''}
         let user_repo = new MemoryUserRepo([u]);
         let q = new GetUsers(user_repo, screen_data);
-        let users = q.execute();
+        let exec = q.execute()
+        expect(exec).toBeInstanceOf(Promise)
+        await exec
+        let users = await exec;
         expect(users.length).toBe(1);
         expect(users[0].username).toBe("u1");
         expect(screen_data.users_list).toBe('u1');
 
     });
-    it("test 3: GetUsers returns the right user", ()=>{
+    it("test 3: GetUsers returns the right user", async ()=>{
         let u = new User("1", "u1", "u1@test.com", "12345678A");
         let u2 = new User("2", "u2", "u2@test.com", "22345678A");
         let screen_data = {users_list:''}
         let user_repo = new MemoryUserRepo([u,u2]);
         let q = new GetUsers(user_repo, screen_data);
-        let users = q.execute();
+        let exec = q.execute()
+        expect(exec).toBeInstanceOf(Promise)
+        await exec
+        let users = await exec;
         expect(users.length).toBe(2);
         expect(users[0].username).toBe("u1");
         expect(screen_data.users_list).toBe('u1, u2');
-        users = q.execute();
+        users = await q.execute();
         expect(users.length).toBe(2);
         expect(users[0].username).toBe("u1");
         expect(screen_data.users_list).toBe('u1, u2');
