99c082a7e689e4235dc492925252ed44eeaa3b21
mirlomusica <mirlomusica@gmail.com>
2026-02-11 13:03:43 +0100

    command API wip. Falta retornar missatges d'error al frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 from typing import Union
+from app.create_user import CreateUser, CreateUserHandler
+from domain.exceptions import UsernameAlreadyExists
+from domain.exceptions import EmailAlreadyExists
+from domain.exceptions import DNIAlreadyExists
 from infra import MemoryUsersRepo
 from app import GetUsers, GetUsersHandler
 from domain import User
@@ -6,6 +10,9 @@ from infra import QueryBus
 import json
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
+from infra.buses import CommandBus
+from fastapi import HTTPException
 
 app = FastAPI()
 app.add_middleware(
@@ -22,8 +29,11 @@ q_bus = QueryBus()
 u = User('1', 'u1', 'u1@test.com', '12345678A')
 repo_user = MemoryUsersRepo([u])
 h = GetUsersHandler(repo_user)
-q = GetUsers()
 q_bus.subscribe(GetUsers, h)
+
+c_bus = CommandBus()
+command_h = CreateUserHandler(repo_user)
+c_bus.subscribe(CreateUser, command_h)
     
 @app.get("/query/{q_name}")
 def read_root(q_name):
@@ -31,5 +41,20 @@ def read_root(q_name):
         return [e.to_primitive() for e in q_bus.dispatch(q_name)]
     except:
         import traceback; traceback.print_exc()
+
+
+@app.post("/command/{c_name}")
+async def write_root(c_name: str, request: Request):
+    
+    try:
+        c_info = await request.json()
+        c_bus.dispatch(c_name,c_info)
+    except UsernameAlreadyExists:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    except EmailAlreadyExists:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    except DNIAlreadyExists:
+        raise HTTPException(status_code=400, detail="DNI already exists")
+        
 
 
